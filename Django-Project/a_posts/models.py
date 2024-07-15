f18bbdd4077e726494e5f53575eaf03713a877cf
Vidyansh07 <Vidyansht@gmail.com>
2024-07-15 20:23:31 +0530

    Adding the model file

--- Django-Project/a_posts/models.py
+++ Django-Project/a_posts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    image = models.URLField(max_length=250)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.CharField(max_length=100, default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    
