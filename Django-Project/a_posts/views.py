873b57d937823633e1c5ee62e7cc25933acdb85e
Vidyansh07 <Vidyansht@gmail.com>
2024-07-15 20:16:15 +0530

    Adding the template

--- Django-Project/a_posts/views.py
+++ Django-Project/a_posts/views.py
@@ -2,5 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_view(request):
-    title = 'Welcome to my blog'
-    return render(request,'index.html', {"title": title})
+    return render(request,'a_posts/home.html')
