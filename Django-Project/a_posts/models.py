4e24d67b7dbe0895a87c1471e3f36ba503749f82
Vidyansh07 <Vidyansht@gmail.com>
2024-07-18 02:27:07 +0530

    Adding the file

--- Django-Project/a_posts/models.py
+++ Django-Project/a_posts/models.py
@@ -5,10 +5,13 @@ import uuid
 
 class Post(models.Model):
     title = models.CharField(max_length=150)
+    artist = models.CharField(max_length=150, null=True)
+    url = models.URLField(max_length=250, null=True)
     image = models.URLField(max_length=250)
     body = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     id = models.CharField(max_length=100, default=uuid.uuid4, unique=True, primary_key=True, editable=False) 
+    
     
     def __str__(self):
         return str(self.title)
--- Django-Project/a_posts/views.py
+++ Django-Project/a_posts/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from .models import Post
 from django.forms import ModelForm
 from django import forms
+from bs4 import BeautifulSoup
+import requests
 # Create your views here.
 def home_view(request):
     posts = Post.objects.all()
@@ -12,12 +14,13 @@ def home_view(request):
 class PostCreateForm(ModelForm):
     class Meta:
         model = Post
-        fields ='__all__'
+        fields =['url','body']
         labels = {
             'body':'Caption',
         }
         widgets = {
             'body': forms.Textarea(attrs={'cols':80, 'rows':3, 'placeholder': 'Add a caption', 'class':'font1 text-4xl'}),
+            'url': forms.FileInput(attrs={'placeholder':'Add the url'})
         }
     
 def post_create_view(request):
@@ -25,6 +28,23 @@ def post_create_view(request):
     if request.method == 'POST':
         form = PostCreateForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            post =form.save(commit=False)
+            
+            website = requests.get(form.data['url'])           
+            sourcecode = BeautifulSoup(website.text, 'html.parser')
+            
+            find_image = sourcecode.select('meta[content^="https://live.staticflickr.com/"]')
+            image = find_image[0].get('content')
+            post.image = image
+            
+            find_title = sourcecode.select('h1.photo-title')
+            title = find_title[0].text.strip()
+            post.title = title
+            
+            find_artist = sourcecode.select('a.owner-name')
+            artist = find_artist[0].text.strip()
+            post.artist = artist           
+            
+            post.save()
             return redirect('home')
     return render(request, 'a_posts/post_create.html', {'form':form})
